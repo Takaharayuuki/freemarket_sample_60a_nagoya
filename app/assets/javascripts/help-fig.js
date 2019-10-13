6c8c552cc5868d829e9de0332a6dfc2f0490a924
hashio111 <mnb7112es@gmail.com>
2019-10-13 12:29:21 +0900

    add fade out function

--- app/assets/javascripts/help-fig.js
+++ app/assets/javascripts/help-fig.js
@@ -2,5 +2,12 @@ $(document).on('turbolinks:load', function() {
   $('#help-text').on('click', function() {
     $('#help-fig').removeClass('hide-help-fig')
   })
-
+  $(document).on('click', function(e){
+    if (!$(e.target).closest('#help-text').length)
+    $('#help-fig').fadeOut();
+  })
+  $('#help-text').on('click', function(e){
+    if (!$(e.target).closest('#help-text').length)
+    $('#help-fig').fadeIn();
+  })
 });
