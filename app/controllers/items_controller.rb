de281c73fa535a24d0f9ce5f68b2fe56fc51107d
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-17 18:22:26 +0900

    商品のデータを保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,33 +7,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
   def create
-    # itemが保存できたかどうかで、画像の保存を分岐させたいために、newです。
     @item = Item.new(create_params)
+    # binding.pry
     if @item.save
-  
-      image_params[:images].each do |image|
-        #buildのタイミングは、newアクションでも可能かもしれません。buildすることで、saveした際にアソシエーション先のテーブルにも値を反映できるようになります。
-        @item.images.build
-        item_image = @item.images.new(image: image)
-        item_image.save
-      end
-        #今回は、Ajaxのみの通信で実装するためHTMLへrespondする必要がないため、jsonのみです。
-      respond_to do |format|
-        format.json
-      end
+      render 'index'
+    else
+      render 'new'
     end
-  end
-  
-  private
-  def create_params
-      # images以外の値についてのストロングパラメータの設定
-      item_params = params.require(:item).permit(:name, :price, :size, :condition, :delivery_fee, :shipping_method, :indication, :burden, :description, :user_id)
-      return item_params
-  end
-  def image_params
-    #imageのストロングパラメータの設定.js側でimagesをrequireすれば画像のみを引き出せるように設定する。
-    params.require(:image).permit({:image => []})
   end
 
   def confirm
@@ -46,6 +28,12 @@ class ItemsController < ApplicationController
       card: params['payjp-token'],
       currency: 'jpy'
     )
+  end
+
+  private
+
+  def create_params
+    params.require(:item).permit(:name, :price, :size, :condition, :delivery_fee, :shipping_method, :indication, :burden, :description, :user_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :status
   belongs_to :brand
+
+  mount_uploader :image, ImageUploader
 end
