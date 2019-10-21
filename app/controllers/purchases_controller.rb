a703e0a2d099aea25a149d5ece0c84f56898d238
hashio111 <mnb7112es@gmail.com>
2019-10-21 16:48:21 +0900

    fix purchase complete page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,7 @@ class PurchasesController < ApplicationController
     @address = current_user.address.prefecture[:name] + current_user.address.city + current_user.address.house_number + current_user.address.building_name 
     @card = current_user.card
     @post_num_first = current_user.address.post_address.to_s.slice(0, 3)
-    @post_num_second = current_user.address.post_address.to_s.slice(3, 7)
+    @post_num_last = current_user.address.post_address.to_s.slice(3, 7)
     if @card
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @card_information = customer.cards.retrieve(@card.card_id)
@@ -13,12 +13,16 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @address = current_user.address.prefecture[:name] + current_user.address.city + current_user.address.house_number + current_user.address.building_name
+    @post_num_first = current_user.address.post_address.to_s.slice(0, 3)
+    @post_num_last = current_user.address.post_address.to_s.slice(3, 7)
     @card = current_user.card
     customer = Payjp::Customer.retrieve(@card.customer_id)
     @card_information = customer.cards.retrieve(@card.card_id)
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-      amount: 300,
+      amount: @item.price,
       customer: @card.customer_id,
       currency: 'jpy'
     )
