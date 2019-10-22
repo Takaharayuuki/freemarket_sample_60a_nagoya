72965fa5e625d19d6f5c262e83158a1d91ec29a0
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-22 11:54:28 +0900

    itemsコントローラー、get_categoryメソッド内の式展開を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,11 +32,11 @@ class ItemsController < ApplicationController
   end
 
   def get_category_children
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    @category_children = Category.find_by(name: params[:parent_name], ancestry: nil).children
   end
 
   def get_category_grandchildren
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   private
