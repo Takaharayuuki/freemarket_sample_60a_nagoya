c48759c68cffaf792a39b5f75752344af1df8359
hashio111 <mnb7112es@gmail.com>
2019-10-24 17:45:01 +0900

    add purchases controller test

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,12 +2,12 @@ class PurchasesController < ApplicationController
   before_action :set_item, only: %i[create]
 
   def new
-    @item = Item.find(20)
+    @item = Item.find(1)
     @address = current_user.address.prefecture[:name] + current_user.address.city + current_user.address.house_number + current_user.address.building_name 
     @card = current_user.card
     @post_num_first = current_user.address.post_address.to_s.slice(0, 3)
     @post_num_last = current_user.address.post_address.to_s.slice(3, 7)
-    if @card
+    if @card.present?
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @card_information = customer.cards.retrieve(@card.card_id)
     end
@@ -18,14 +18,18 @@ class PurchasesController < ApplicationController
     @post_num_first = current_user.address.post_address.to_s.slice(0, 3)
     @post_num_last = current_user.address.post_address.to_s.slice(3, 7)
     @card = current_user.card
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-    @card_information = customer.cards.retrieve(@card.card_id)
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    Payjp::Charge.create(
-      amount: @item.price,
-      customer: @card.customer_id,
-      currency: 'jpy'
-    )
+    if @card.present?
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_information = customer.cards.retrieve(@card.card_id)
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+        amount: @item.price,
+        customer: @card.customer_id,
+        currency: 'jpy'
+      )
+    else
+      render :new
+    end
   end
 
   private
