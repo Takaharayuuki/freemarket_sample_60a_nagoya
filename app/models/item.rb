d66223b0334b07627d7127dd1cf3b6c01df19094
hashio111 <mnb7112es@gmail.com>
2019-10-24 19:29:56 +0900

    fix item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   # has_many_attached :images, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  # belongs_to :category
+  belongs_to :category
   belongs_to :user
   # belongs_to :status
   # belongs_to :brand
