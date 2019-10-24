72cec8f37c5fe1ef14ef69122a73162f4aeae305
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-24 19:16:32 +0900

    itemsコントローラーのcreateアクション内のbinding.pryを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @item = Item.new(create_params)
     if @item.save
       redirect_to controller: :items, action: :index
