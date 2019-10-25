54212b2bcf52d125c5f19d033dc46dd339c2bf97
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-25 19:26:42 +0900

    s3へ画像を保存するためのファイルを編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :images
   belongs_to :user, optional:true
-  belongs_to :category
+  # belongs_to :category
 
   with_options presence: true do
-    validates :category_id
+    # validates :category_id
     validates :condition
     validates :burden
     validates :shipping_method
