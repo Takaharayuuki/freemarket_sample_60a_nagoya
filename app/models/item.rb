520e4b760e2704782ae48dc9a5aa282aeaff7cb6
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-25 16:39:24 +0900

    共通したバリデーションを一つにまとめる。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :user
+  belongs_to :user, optional:true
 
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :condition, presence: true
-  validates :burden, presence: true
-  validates :shipping_method, presence: true
-  validates :indication, presence: true
-  validates :price, presence: true,numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
-  # validates :size, presence: true
-  # validates :delivery_fee, presence: true
+  with_options presence: true do
+    validates :category_id
+    validates :condition
+    validates :burden
+    validates :shipping_method
+    validates :indication
+    validates :price
+    # validates :size
+    # validates :delivery_fee
+  end
+  
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
 
   # has_many :comments, dependent: :destroy
   # has_many :likes, dependent: :destroy
