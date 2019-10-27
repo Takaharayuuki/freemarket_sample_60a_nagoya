f95c949cb81f8284188cf441b0637ecc3eba8cf5
hashio111 <mnb7112es@gmail.com>
2019-10-27 15:31:04 +0900

    modified purchases_controller test

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -4,10 +4,11 @@ describe PurchasesController, type: :controller do
 
   describe 'GET #new' do
     it "renders the :new template" do
+      category = create(:category)
       user = create(:user)
       address = create(:address, user: user)
       sign_in(user)
-      item = create(:item, user: user)
+      item = create(:item, user: user, category: category, saler: user)
       get :new, params: {item_id: item.id}
       expect(response).to render_template :new
     end
@@ -15,10 +16,11 @@ describe PurchasesController, type: :controller do
 
   describe 'POST #create' do
     it "match the :create purchase" do
+      category = create(:category)
       user = create(:user)
       address = create(:address, user: user)
       sign_in(user)
-      item = create(:item, user: user)
+      item = create(:item, user: user, category: category, saler: user)
       post :create, params: {item_id: item.id}
       expect(response).to render_template :new
     end
