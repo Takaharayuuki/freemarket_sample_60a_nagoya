a808bb7d7bf192603202d7b9521f1e5e4da56349
hashio111 <mnb7112es@gmail.com>
2019-10-27 20:13:51 +0900

    modified height

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,11 @@
 class SearchesController < ApplicationController
   def index
-    @key_word = params[:q][:name_cont]
-    @search_items = Item.order('created_at DESC').ransack(params[:q])
-    @items = @search_items.result
+    if params[:q].blank?
+      redirect_to root_path
+    else
+      @key_word = params[:q][:name_cont]
+      @search_items = Item.order('created_at DESC').ransack(params[:q])
+      @items = @search_items.result
+    end
   end
 end
