bb672d9e99915ae1268fb98d444359df7052c41a
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-10-28 18:16:35 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,6 +48,9 @@ class ItemsController < ApplicationController
     @prefecture = Prefecture.all
     @category = Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
+    end
+  end
+  
   def destroy
     item = Item.find(params[:id])
     if item.destroy
